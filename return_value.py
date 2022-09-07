779caefe5a69696993078dd83de845b08755609b
zsofia8893 <kovacs.zsofia8893@gmail.com>
2022-09-07 14:47:49 +0200

    version4

--- return_value.py
+++ return_value.py
@@ -15,3 +15,86 @@ def if_bigger_0(n):
 print(if_bigger_0(1))
 print(if_bigger_0(-1))
 
+
+print("----")
+
+# Írjatok egy függvényt, ami vár egy egész számot, és visszaadja
+# annak abszolútértékét!
+
+def absolut(c):
+    if c < 0:
+        result = -c
+    else:
+        result = c
+    return result    #struktúrált programozás: minden függvényben CSAK egy return utasítás lehet.
+
+print(absolut(-1))
+print(absolut(2))
+
+
+
+# Írjatok egy függvényt, mely várja a téglalap a és b oldalát, és
+# visszaadja a területet!
+def area(a, b):
+    return a * b
+
+print(area(3,2))
+
+
+# Írjatok egy függvényt, mely várja a téglalap a és b oldalát, és
+# visszaadja a kerületet!
+def calculate_perimeter(a, b):
+    return 2 * (a + b)
+
+print(calculate_perimeter(4, 6))
+
+
+# Írjatok egy függvényt, amely vár két számot, és visszaadja a
+# kettő közül a nagyobbat!
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print(max(6, 9))
+print(max(5, 3))
+
+
+# Vár egy számot és visszaadja a "páros" szöveget, ha páros,
+# ellenkező esetben hogy "páratlan"
+def get_type(n):
+    if n % 2 == 0:  # % csak a maradékos osztás, itt a maradéknak 0-nak kell lennie
+        return "páros"
+    else:
+        return "páratlan"
+
+print(get_type(6))
+print(get_type(5))
+
+#Ha a függvény boolean értéket ad vissza, akkor logikai függvény: True or False
+
+#írj egy is_even nevű függvényt mely a paraméteréről eldönti hogy páros-e
+#True ha páros, False ha páratlan
+
+def is_even(n):
+    if n % 2 == 0: #elosztjuk 2-vel ÉS a mardéka nulla
+        return True
+    else:
+        return False
+
+#EGYENŐ ezzel;
+
+def is_even_simple(n):
+    return n % 2 == 0
+
+print(is_even(6))
+
+print(is_even_simple(8))
+
+if is_even(5):
+    print("Ez egy páros szám")
+else:
+    print("Ez egy páros szám!")
+
+print(is_even(5))
