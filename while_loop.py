6b7b7de44fc333409d90516dbce2314e1400c0c5
zsofia8893 <kovacs.zsofia8893@gmail.com>
2022-09-07 12:04:15 +0200

    version2

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+#while True:
+    print("Hello World!") #végtelen ciklus
+
+#nem fut le a ciklus törzse while False:
+    print("One")
+#Print("Two")
+
+i = 0
+#while i < 5: #amíg a feltétel igaz, addig lefuttatja a ciklustörzset
